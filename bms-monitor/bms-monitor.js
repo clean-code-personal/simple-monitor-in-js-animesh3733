ef557f916ea13293c61f2a09a74e9aed5f7d2c64
animesh3733 <85788656+animesh3733@users.noreply.github.com>
2023-03-03 11:28:14 +0530

    Add files via upload

---
+++ bms-monitor/bms-monitor.js
@@ -0,0 +1,25 @@
+const { expect } = require('chai');
+
+function isOutOfRange(value, min, max) {
+    return value < min || value > max;
+}
+
+function batteryIsOk(temperature, soc, charge_rate) {
+    const temperatureOutOfRange = isOutOfRange(temperature, 0, 45);
+    const socOutOfRange = isOutOfRange(soc, 20, 80);
+    const chargeRateOutOfRange = isOutOfRange(charge_rate, 0, 0.8);
+    
+    if (temperatureOutOfRange) {
+        console.log('Temperature is out of range!');
+    }
+
+    if (socOutOfRange) {
+        console.log('State of Charge is out of range!');
+    }
+
+    if (chargeRateOutOfRange) {
+        console.log('Charge rate is out of range!');
+    }
+
+    return !temperatureOutOfRange && !socOutOfRange && !chargeRateOutOfRange;
+}
