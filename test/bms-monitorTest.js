4f0bc66eaff2dd3e1c64decb65189f23a60b5f9e
animesh3733 <85788656+animesh3733@users.noreply.github.com>
2023-03-03 11:26:45 +0530

    Add files via upload

---
+++ test/bms-monitorTest.js
@@ -0,0 +1,9 @@
+expect(batteryIsOk(25, 70, 0.7)).to.be.true;
+expect(batteryIsOk(-10, 70, 0.7)).to.be.false;
+expect(batteryIsOk(50, 70, 0.7)).to.be.false;
+expect(batteryIsOk(25, 10, 0.7)).to.be.false;
+expect(batteryIsOk(25, 90, 0.7)).to.be.false;
+expect(batteryIsOk(25, 70, 1)).to.be.false;
+expect(batteryIsOk(25, 70, 0.8)).to.be.true;
+expect(batteryIsOk(0, 20, 0)).to.be.true;
+expect(batteryIsOk(45, 80, 0.8)).to.be.true;
