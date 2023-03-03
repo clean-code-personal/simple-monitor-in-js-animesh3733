30c7d9cb9e67e8e817a135494e777d9997360dcd
animesh3733 <85788656+animesh3733@users.noreply.github.com>
2023-03-03 09:27:42 +0530

    Update bms-monitor.js

--- bms-monitor.js
+++ bms-monitor.js
@@ -25,4 +25,11 @@ function batteryIsOk(temperature, soc, charge_rate) {
 }
 
 expect(batteryIsOk(25, 70, 0.7)).to.be.true;
-expect(batteryIsOk(50, 85, 0)).to.be.false;
+expect(batteryIsOk(-10, 70, 0.7)).to.be.false;
+expect(batteryIsOk(50, 70, 0.7)).to.be.false;
+expect(batteryIsOk(25, 10, 0.7)).to.be.false;
+expect(batteryIsOk(25, 90, 0.7)).to.be.false;
+expect(batteryIsOk(25, 70, 1)).to.be.false;
+expect(batteryIsOk(25, 70, 0.8)).to.be.true;
+expect(batteryIsOk(0, 20, 0)).to.be.true;
+expect(batteryIsOk(45, 80, 0.8)).to.be.true;
