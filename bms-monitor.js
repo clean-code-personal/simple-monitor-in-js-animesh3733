3e5ef9f65190014e8b082a551ec507682ebf70d8
animesh3733 <85788656+animesh3733@users.noreply.github.com>
2023-03-03 11:23:11 +0530

    Update bms-monitor.js

--- bms-monitor.js
+++ bms-monitor.js
@@ -1,42 +1,26 @@
+const { expect } = require('chai');
 
-function isTemperatureOutOfRange(temperature) {
-    return temperature < 0 || temperature > 45;
-}
-
-function isSocOutOfRange(soc) {
-    return soc < 20 || soc > 80;
-}
-
-function isChargeRateOutOfRange(charge_rate) {
-    return charge_rate > 0.8;
+function isOutOfRange(value, min, max) {
+    return value < min || value > max;
 }
 
 function batteryIsOk(temperature, soc, charge_rate) {
-    if (isTemperatureOutOfRange(temperature)) {
+    const temperatureOutOfRange = isOutOfRange(temperature, 0, 45);
+    const socOutOfRange = isOutOfRange(soc, 20, 80);
+    const chargeRateOutOfRange = isOutOfRange(charge_rate, 0, 0.8);
+    
+    if (temperatureOutOfRange) {
         console.log('Temperature is out of range!');
-        return false;
     }
 
-    if (isSocOutOfRange(soc)) {
+    if (socOutOfRange) {
         console.log('State of Charge is out of range!');
-        return false;
     }
 
-    if (isChargeRateOutOfRange(charge_rate)) {
+    if (chargeRateOutOfRange) {
         console.log('Charge rate is out of range!');
-        return false;
     }
 
-    return true;
+    return !temperatureOutOfRange && !socOutOfRange && !chargeRateOutOfRange;
 }
 
-
-expect(batteryIsOk(25, 70, 0.7)).to.be.true;
-expect(batteryIsOk(-10, 70, 0.7)).to.be.false;
-expect(batteryIsOk(50, 70, 0.7)).to.be.false;
-expect(batteryIsOk(25, 10, 0.7)).to.be.false;
-expect(batteryIsOk(25, 90, 0.7)).to.be.false;
-expect(batteryIsOk(25, 70, 1)).to.be.false;
-expect(batteryIsOk(25, 70, 0.8)).to.be.true;
-expect(batteryIsOk(0, 20, 0)).to.be.true;
-expect(batteryIsOk(45, 80, 0.8)).to.be.true;
