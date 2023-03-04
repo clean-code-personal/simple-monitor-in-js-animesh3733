ac935f9f1587d3c6f971dd83f8fbd465191bede4
animesh3733 <85788656+animesh3733@users.noreply.github.com>
2023-03-04 21:34:55 +0530

    Update bms-monitorTest.js

--- test/bms-monitorTest.js
+++ test/bms-monitorTest.js
@@ -1,4 +1,5 @@
 import { batteryIsOk } from '../bms-monitor/bms-monitor.js';
+const { expect } = require('chai');
 
 expect(batteryIsOk(25, 70, 0.7)).to.be.true;
 expect(batteryIsOk(-10, 70, 0.7)).to.be.false;
