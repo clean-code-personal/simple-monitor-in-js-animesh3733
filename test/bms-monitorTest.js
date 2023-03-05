6e2c610fa0bf140801d7e0fbd36eb0d395c293ce
animesh3733 <85788656+animesh3733@users.noreply.github.com>
2023-03-05 23:55:01 +0530

    Update bms-monitorTest.js

--- test/bms-monitorTest.js
+++ test/bms-monitorTest.js
@@ -1,12 +1,25 @@
 import { batteryIsOk } from '../bms-monitor/bms-monitor.js';
 import { expect } from 'chai';
 
-expect(batteryIsOk(25, 70, 0.7)).to.be.true;
-expect(batteryIsOk(-10, 70, 0.7)).to.be.false;
-expect(batteryIsOk(50, 70, 0.7)).to.be.false;
-expect(batteryIsOk(25, 10, 0.7)).to.be.false;
-expect(batteryIsOk(25, 90, 0.7)).to.be.false;
-expect(batteryIsOk(25, 70, 1)).to.be.false;
-expect(batteryIsOk(25, 70, 0.8)).to.be.true;
-expect(batteryIsOk(0, 20, 0)).to.be.true;
-expect(batteryIsOk(45, 80, 0.8)).to.be.true;
+describe('batteryIsOk', () => {
+  it('should return true when all parameters are within the normal range', () => {
+    expect(batteryIsOk(25, 70, 0.7)).to.be.true;
+  });
+
+  it('should return false when temperature is below lower limit', () => {
+    expect(batteryIsOk(-10, 70, 0.7)).to.be.false;
+  });
+
+  it('should return false when temperature is above upper limit', () => {
+    expect(batteryIsOk(50, 70, 0.7)).to.be.false;
+  });
+
+  it('should return false when soc is below lower limit', () => {
+    expect(batteryIsOk(25, 10, 0.7)).to.be.false;
+  });
+
+  it('should return false when soc is above upper limit', () => {
+    expect(batteryIsOk(25, 90, 0.7)).to.be.false;
+  });
+});
+
