c9e649b9287d37dedc24e2416ac60e8f7be7795d
animesh3733 <85788656+animesh3733@users.noreply.github.com>
2023-03-05 23:54:34 +0530

    Update bms-monitor.js

--- bms-monitor/bms-monitor.js
+++ bms-monitor/bms-monitor.js
@@ -1,5 +1,39 @@
-export function isOutOfRange(value, lowerLimit, upperLimit) {
+const WARNING_TOLERANCE_PERCENT = 5;
+
+function isOutOfRange(value, lowerLimit, upperLimit) {
   return value < lowerLimit || value > upperLimit;
+}
+
+function getBreachLevel(value, lowerLimit, upperLimit, warningTolerancePercent) {
+  const range = upperLimit - lowerLimit;
+  const warningTolerance = range * warningTolerancePercent / 100;
+  if (value < lowerLimit) {
+    return 'LOW_BREACH';
+  } else if (value <= lowerLimit + warningTolerance) {
+    return 'LOW_WARNING';
+  } else if (value >= upperLimit - warningTolerance) {
+    return 'HIGH_WARNING';
+  } else if (value > upperLimit) {
+    return 'HIGH_BREACH';
+  } else {
+    return 'NORMAL';
+  }
+}
+
+function getBatteryStatusMessage(breachLevels) {
+  let message;
+  if (breachLevels.some(level => level === 'HIGH_BREACH')) {
+    message = 'High breach';
+  } else if (breachLevels.some(level => level === 'HIGH_WARNING')) {
+    message = 'Approaching charge-peak';
+  } else if (breachLevels.some(level => level === 'LOW_BREACH')) {
+    message = 'Low breach';
+  } else if (breachLevels.some(level => level === 'LOW_WARNING')) {
+    message = 'Approaching discharge';
+  } else {
+    message = 'Normal';
+  }
+  return message;
 }
 
 export function batteryIsOk(temperature, soc, charge_rate) {
@@ -9,13 +43,14 @@ export function batteryIsOk(temperature, soc, charge_rate) {
     { name: 'Charge rate', value: charge_rate, lowerLimit: 0, upperLimit: 0.8 }
   ];
 
-  for (let parameter of parameters) {
-    if (isOutOfRange(parameter.value, parameter.lowerLimit, parameter.upperLimit)) {
-      console.log(`${parameter.name} is out of range!`);
-      return false;
-    }
-  }
-  
-  return true;
+  const breachLevels = parameters.map(parameter => {
+    const warningTolerancePercent = parameter.upperLimit * WARNING_TOLERANCE_PERCENT / 100;
+    return getBreachLevel(parameter.value, parameter.lowerLimit, parameter.upperLimit, warningTolerancePercent);
+  });
+
+  const batteryStatusMessage = getBatteryStatusMessage(breachLevels);
+  console.log(`Battery status: ${batteryStatusMessage}`);
+
+  return !breachLevels.some(level => level === 'LOW_BREACH' || level === 'HIGH_BREACH');
 }
 
