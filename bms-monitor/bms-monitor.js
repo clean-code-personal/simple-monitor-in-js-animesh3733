8177d8d4ab8d1f36707fb3c901ac62be1d49f2ad
animesh3733 <85788656+animesh3733@users.noreply.github.com>
2023-03-06 14:16:15 +0530

    Update bms-monitor.js

--- bms-monitor/bms-monitor.js
+++ bms-monitor/bms-monitor.js
@@ -7,33 +7,23 @@ function isOutOfRange(value, lowerLimit, upperLimit) {
 function getBreachLevel(value, lowerLimit, upperLimit, warningTolerancePercent) {
   const range = upperLimit - lowerLimit;
   const warningTolerance = range * warningTolerancePercent / 100;
-  if (value < lowerLimit) {
-    return 'LOW_BREACH';
-  } else if (value <= lowerLimit + warningTolerance) {
-    return 'LOW_WARNING';
-  } else if (value >= upperLimit - warningTolerance) {
-    return 'HIGH_WARNING';
-  } else if (value > upperLimit) {
-    return 'HIGH_BREACH';
+  if (isOutOfRange(value, lowerLimit - warningTolerance, upperLimit + warningTolerance)) {
+    return value < lowerLimit ? 'LOW_BREACH' : 'HIGH_BREACH';
   } else {
     return 'NORMAL';
   }
 }
 
 function getBatteryStatusMessage(breachLevels) {
-  let message;
-  if (breachLevels.some(level => level === 'HIGH_BREACH')) {
-    message = 'High breach';
-  } else if (breachLevels.some(level => level === 'HIGH_WARNING')) {
-    message = 'Approaching charge-peak';
-  } else if (breachLevels.some(level => level === 'LOW_BREACH')) {
-    message = 'Low breach';
-  } else if (breachLevels.some(level => level === 'LOW_WARNING')) {
-    message = 'Approaching discharge';
-  } else {
-    message = 'Normal';
-  }
-  return message;
+  const levels = {
+    'HIGH_BREACH': 'High breach',
+    'HIGH_WARNING': 'Approaching charge-peak',
+    'LOW_BREACH': 'Low breach',
+    'LOW_WARNING': 'Approaching discharge',
+    'NORMAL': 'Normal'
+  };
+  const messages = breachLevels.map(level => levels[level]);
+  return messages.join(' and ');
 }
 
 export function batteryIsOk(temperature, soc, charge_rate) {
@@ -53,4 +43,3 @@ export function batteryIsOk(temperature, soc, charge_rate) {
 
   return !breachLevels.some(level => level === 'LOW_BREACH' || level === 'HIGH_BREACH');
 }
-
